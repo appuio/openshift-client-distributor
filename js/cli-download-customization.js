bb0d5719a389e2d36fc65eca2f9de25d07c35ead
Martin Abegglen <abegglen@puzzle.ch>
2019-06-28 16:26:21 +0200

    Remove old deployment strategy

--- js/cli-download-customization.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-window.OPENSHIFT_CONSTANTS.CLI = {
-  "Linux":    "https://client.ocp.puzzle.ch/linux/oc",
-  "Windows":  "https://client.ocp.puzzle.ch/windows/oc.exe",
-  "Mac OS X": "https://client.ocp.puzzle.ch/macosx/oc"
-};
