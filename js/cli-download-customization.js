721a19d72b9874eba42fb8f2fc3c8c2148728fcb
Benjamin Affolter <bliemli@users.noreply.github.com>
2019-06-28 14:46:48 +0200

    Change URLs to new subdomain

--- js/cli-download-customization.js
+++ js/cli-download-customization.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.OPENSHIFT_CONSTANTS.CLI = {
-  "Linux":    "https://client.ose3.puzzle.ch/linux/oc",
-  "Windows":  "https://client.ose3.puzzle.ch/windows/oc.exe",
-  "Mac OS X": "https://client.ose3.puzzle.ch/macosx/oc"
+  "Linux":    "https://client.ocp.puzzle.ch/linux/oc",
+  "Windows":  "https://client.ocp.puzzle.ch/windows/oc.exe",
+  "Mac OS X": "https://client.ocp.puzzle.ch/macosx/oc"
 };
